2f321c7996c5026d226c14a335cc5aa942600d5e
Dan Sebag <dan.sebag1007@gmail.com>
2023-01-14 19:36:11 +0100

    Modification of code architecture

---
+++ MarioBros/include/headers/game.h
@@ -0,0 +1,21 @@
+#include <SDL_video.h>
+#include "define.h"
+
+typedef struct Player Player;
+typedef SDL_Renderer Renderer;
+
+struct Player {
+    SDL_Texture *texture;
+    SDL_Rect rect;
+    int pv;
+};
+
+void initPlayer(Player *player, Renderer *renderer, const char *imagePath, int pv);
+
+void createBackground(SDL_Renderer *renderer, const char *imagePath);
+
+void handlePlayerMovement(Player *player, SDL_Event event, Renderer *renderer);
+
+void createPlayer(SDL_Renderer *renderer, const char *imagePath);
+
+int playerMove(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Rect playerRect, int type);
