e110a9f3fd31c79d628ecf712432d3dc3f83504d
Dan Sebag <dan.sebag1007@gmail.com>
2023-01-21 18:06:19 +0100

    Update enemy and jump + move

--- MarioBros/src/game.c
+++ MarioBros/src/game.c
@@ -21,7 +21,7 @@ void initPlayer(Player *player, SDL_Renderer *renderer, const char *imagePath, i
     }
 
     // Initialize player rect
-    player->rect.x = 0;
+    player->rect.x = 15;
     player->rect.y = 165;
     player->rect.w = 16;
     player->rect.h = 32;
@@ -34,6 +34,72 @@ void initPlayer(Player *player, SDL_Renderer *renderer, const char *imagePath, i
 
     // Free memory
     SDL_FreeSurface(playerImage);
+}
+
+void createEnemy(Enemy *enemy, SDL_Renderer *renderer, const char *imagePath) {
+    // Charger l'image de l'ennemi
+    SDL_Surface *enemyImage = SDL_LoadBMP(imagePath);
+
+    // Redimensionner l'image de l'ennemi
+    SDL_Surface *enemyResize = SDL_CreateRGBSurface(0, 16, 16, 32, 0, 0, 0, 0);
+
+    SDL_BlitScaled(enemyImage, NULL, enemyResize, NULL);
+
+    // Rendre le vert transparent
+    SDL_SetColorKey(enemyResize, SDL_TRUE, SDL_MapRGB(enemyResize->format, 0, 255, 0));
+
+    // Créer une texture à partir de l'image
+    enemy->texture = SDL_CreateTextureFromSurface(renderer, enemyResize);
+    if (enemy->texture == NULL) {
+        printf("Could not create texture: %s\n", SDL_GetError());
+        return;
+    }
+
+    // Initialiser le rectangle de l'ennemi
+    enemy->rect.x = 300;
+    enemy->rect.y = 180;
+    enemy->rect.w = enemyImage->w;
+    enemy->rect.h = enemyImage->h;
+
+    // Initialiser la vitesse de déplacement de l'ennemi
+    enemy->x_velocity = 5;
+
+    // Afficher l'ennemi à l'écran
+    SDL_RenderCopy(renderer, enemy->texture, NULL, &enemy->rect);
+    SDL_RenderPresent(renderer);
+
+    // Libérer la mémoire
+    SDL_FreeSurface(enemyImage);
+}
+
+// je veux une fonction qui récupère seulement la position en x du joueur
+int playerPositionX(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Rect playerRect) {
+
+    int x = playerRect.x;
+
+    return x;
+}
+
+void moveEnemy(Enemy *enemy, SDL_Renderer *renderer, Player *player) {
+
+    SDL_RenderClear(renderer);
+
+    if (enemy->rect.x >= 300 && enemy->rect.x <= 400) {
+        enemy->rect.x += enemy->x_velocity;
+    } else {
+        enemy->x_velocity = -enemy->x_velocity;
+        enemy->rect.x += enemy->x_velocity;
+    }
+    // Create background
+    createBackground(renderer, "../include/ressources/images/background/desert/desert1.bmp", 0, 0 , WIDTH, HEIGHT);
+
+    SDL_RenderCopy(renderer, enemy->texture, NULL, &enemy->rect);
+
+    SDL_RenderCopy(renderer, player->texture, NULL, &player->rect);
+    SDL_RenderPresent(renderer);
+
+    SDL_Delay(100);
+
 }
 
 
@@ -62,117 +128,96 @@ void createBackground(SDL_Renderer *renderer, const char *imagePath, int x, int 
     SDL_FreeSurface(background);
 }
 
-int playerMove(SDL_Renderer *renderer, SDL_Texture *texture, SDL_Rect playerRect, int type) {
+bool keys[SDL_NUM_SCANCODES];
+bool isJumping = false;
+bool isMovingRight = false;
 
-    // On efface le joueur à l'écran
+void handlePlayerMovement(Player *player, SDL_Event event, SDL_Renderer *renderer, Enemy *enemy) {
+    // check for key press events
+    if (event.type == SDL_KEYDOWN) {
+
+        if (event.key.keysym.sym == SDLK_RIGHT) {
+            keys[SDL_SCANCODE_RIGHT] = true;
+            if (player->rect.x < WIDTH - player->rect.w) {
+                player->rect.x += 10;
+            }
+            isMovingRight = true;
+        }
+        if (event.key.keysym.sym == SDLK_LEFT) {
+            keys[SDL_SCANCODE_LEFT] = true;
+            if (player->rect.x > 15) {
+                player->rect.x -= 10;
+            }
+        }
+        if (event.key.keysym.sym == SDLK_SPACE) {
+            keys[SDL_SCANCODE_SPACE] = true;
+            if (player->rect.y == 165 && !isJumping) {
+                player->rect.y -= 30;
+            }
+            isJumping = true;
+        }
+
+        // Si key.right est true et que key.space est true, alors le joueur saute et va à droite en même temps
+        if (keys[SDL_SCANCODE_RIGHT] && keys[SDL_SCANCODE_SPACE] && isMovingRight) {
+            if (player->rect.x < WIDTH - player->rect.w) {
+                player->rect.x += 10;
+            }
+            if (player->rect.y == 165 && !isJumping) {
+                player->rect.y -= 30;
+            }
+            isJumping = true;
+        }
+    }
+        // check for key release events
+    else if (event.type == SDL_KEYUP) {
+        if (event.key.keysym.sym == SDLK_RIGHT) {
+            keys[SDL_SCANCODE_RIGHT] = false;
+            isMovingRight = false;
+        }
+        if (event.key.keysym.sym == SDLK_LEFT) {
+            keys[SDL_SCANCODE_LEFT] = false;
+        }
+        if (event.key.keysym.sym == SDLK_SPACE) {
+            keys[SDL_SCANCODE_SPACE] = false;
+        }
+    }
+
+    if (player->pv == 0) {
+        SDL_EventState(SDL_KEYDOWN, SDL_IGNORE);
+        playerIsDead(renderer, "../include/ressources/images/background/Gameover.bmp");
+    }
+
+    // clear the renderer
     SDL_RenderClear(renderer);
 
-    SDL_Rect camera = {0, 0, WIDTH, HEIGHT};
+    // move the enemy
+    moveEnemy(enemy, renderer, player);
 
-    switch (type) {
+    // render the player's texture
+    SDL_RenderCopy(renderer, player->texture, NULL, &player->rect);
 
-        case 1:
-
-            // On déplace le joueur de 10 pixels vers la droite
-            playerRect.x += 10;
-            playerRect.y = 165;
-
-            createBackground(renderer, "../include/ressources/images/background/desert/desert1.bmp", camera.x, camera.y, WIDTH, HEIGHT);
-
-            SDL_RenderCopy(renderer, texture, NULL, &playerRect);
-            SDL_RenderPresent(renderer);
-
-            break;
-
-        case 2:
-
-            playerRect.x -= 10;
-            playerRect.y = 165;
-
-            createBackground(renderer, "../include/ressources/images/background/desert/desert1.bmp", camera.x, camera.y, WIDTH, HEIGHT);
-            // Flip the player
-            SDL_RenderCopyEx(renderer, texture, NULL, &playerRect, 0, NULL, SDL_FLIP_HORIZONTAL);
-            SDL_RenderPresent(renderer);
-
-            break;
-
-        case 3:
-
-            playerRect.y -= 30;
-            // Garder sa position en x
-            int x = playerRect.x;
-
-            createBackground(renderer, "../include/ressources/images/background/desert/desert1.bmp", camera.x, camera.y, WIDTH, HEIGHT);
-            SDL_RenderCopy(renderer, texture, NULL, &playerRect);
-            SDL_RenderPresent(renderer);
-
-            // On replace le joueur à sa position en x
-            playerRect.x = x;
-
-            break;
-
-        default:
-
-            break;
-
+    // Si le joueur n'est pas sur le sol, il tombe
+    if (player->rect.y < 165) {
+        player->rect.y += 15;
     }
 
-    return playerRect.x;
-}
-
-int stateSpace = 0;
-
-void handlePlayerMovement(Player *player, SDL_Event event, SDL_Renderer *renderer)
-{
-    if (event.type == SDL_KEYDOWN) {
-        if (event.key.keysym.sym == SDLK_RIGHT) {
-            int type = 1;
-            SDL_Delay(150);
-            if (player->rect.x < WIDTH - player->rect.w) {
-                player->rect.x = playerMove(renderer, player->texture, player->rect, type);
-            }
-        }
-        if(event.key.keysym.sym == SDLK_LEFT) {
-            int type = 2;
-            SDL_Delay(150);
-            if (player->rect.x > 0) {
-                player->rect.x = playerMove(renderer, player->texture, player->rect, type);
-            }
-        }
-        if(event.key.keysym.sym == SDLK_SPACE) {
-
-            if (player->rect.y == 165 && stateSpace == 0) {
-                int type = 3;
-                SDL_Delay(150);
-                player->rect.y = playerMove(renderer, player->texture, player->rect, type);
-                SDL_Delay(400);
-
-            }else{
-                stateSpace = 1;
-            }
-
-            // Clear player texture
-            SDL_RenderClear(renderer);
-            createBackground(renderer, "../include/ressources/images/background/desert/desert1.bmp", 0, 0, WIDTH, HEIGHT);
-            player->rect.y = 165;
-            SDL_RenderCopy(renderer, player->texture, NULL, &player->rect);
-            SDL_RenderPresent(renderer);
-
-            SDL_Delay(150);
-            stateSpace = 0;
-
-        }
-
-        if (player->pv == 0){
-            SDL_EventState(SDL_KEYDOWN, SDL_IGNORE);
-            playerIsDead(renderer, "../include/ressources/images/background/Gameover.bmp");
-        }
+    // Si le joueur est sur le sol, il ne peut plus sauter
+    if (player->rect.y >= 165) {
+        isJumping = false;
     }
 }
 
-void createPlayer(SDL_Renderer *renderer, const char *imagePath) {
+
+void loopGame(SDL_Renderer *renderer) {
+
     Player player;
-    initPlayer(&player, renderer, imagePath, 100);
+    initPlayer(&player, renderer, "../include/ressources/images/sprite/mario/mario_normal_1.bmp", 100);
+
+    Enemy enemy;
+    createEnemy(&enemy, renderer, "../include/ressources/images/sprite/goomba/goomba_1.bmp");
+
+    createBackground(renderer, "../include/ressources/images/background/desert/desert1.bmp", 0, 0, WIDTH, HEIGHT);
+
 
     SDL_Event event;
 
@@ -184,8 +229,13 @@ void createPlayer(SDL_Renderer *renderer, const char *imagePath) {
                 exit(0);
                 return;
             }
-            handlePlayerMovement(&player, event, renderer);
+
+            handlePlayerMovement(&player, event, renderer, &enemy);
         }
+
+        // Déplacement de l'ennemi
+        moveEnemy(&enemy, renderer, &player);
+
     }
 }
 
